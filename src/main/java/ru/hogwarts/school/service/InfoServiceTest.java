7c5854caa6f2cebe5237da8979bdc582152cf52f
aleks <aleksei-davletov@mail.ru>
2023-12-18 14:03:01 +0300

    Добавил еще один профиль с другим значением server.port

--- src/main/java/ru/hogwarts/school/service/InfoServiceTest.java
+++ src/main/java/ru/hogwarts/school/service/InfoServiceTest.java
@@ -2,6 +2,7 @@ package ru.hogwarts.school.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +12,11 @@ public class InfoServiceTest implements InfoService{
 
     Logger logger = LoggerFactory.getLogger(InfoServiceTest.class);
 
+    @Value("${server.port}")
+    private String port;
+
     public String getPort() {
         logger.debug("Вызван метод getPort");
-        return "1111";
+        return port;
     }
 }
